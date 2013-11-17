a68f575f3b98f70c5436370d3bc2393babf06d3a
Jaime Freire <jaime.freire@gmail.com>
2013-11-17 22:34:30 +0100

    Validation changes and new entities

--- grails-app/domain/database/Usuario.groovy
+++ grails-app/domain/database/Usuario.groovy
@@ -1,8 +1,25 @@
 package database
 class Usuario {
-    String name
+    String nombre
+    String apellidos
+    String username
+    String password
+    String correo
+    boolean activo
+    Date fecha_nacimiento
 
-    String toString() { name }
+    static constraints = {
+        nombre(blank:false,size:4..50)
+        apellidos(blank:false,size:4..50)
+        username(blank:false,unique: true,size:4..50)
+        password(password:false,size:4..50)
+        correo(blank:false,email,size:4..50)
+        fecha_nacimiento(blank:false)
+
+    }
+
+
+    String toString() { username }
 
     static hasMany = [medias: Media]
 
